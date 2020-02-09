58b02e097d5e4b2a1e12074a67d83f58e61e9461
guilhermematheuscorrea <guilherme.matheus.correa@gmail.com>
2020-02-09 17:32:43 -0300

    feat: Send mail to Mailtrap

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'a99d5d9f06781f',
+    pass: 'f8599d1db93834',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
