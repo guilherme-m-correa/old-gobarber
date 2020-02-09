8f300444b2fc54f58fc4cfe1c3071c669789433a
guilhermematheuscorrea <guilherme.matheus.correa@gmail.com>
2020-02-09 00:46:45 -0300

    fix: Appointment validation

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -33,6 +33,7 @@ class AppointmentController {
      * Check for past dates
      */
     const hourStart = startOfHour(parseISO(date));
+
     if (isBefore(hourStart, new Date())) {
       return res.status(400).json({ error: 'Past dates are not allowed' });
     }
@@ -57,7 +58,7 @@ class AppointmentController {
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
-      date,
+      date: hourStart,
     });
 
     return res.json(appointment);
