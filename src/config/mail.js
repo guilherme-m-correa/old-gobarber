208f1c8ed42a21ffdc6c046ea343d27d719b0f15
guilhermematheuscorrea <guilherme.matheus.correa@gmail.com>
2020-02-25 22:32:22 -0300

    refactor: Add .env

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '62b4235e210325',
-    pass: 'a30e2ae9a7a1b8',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://94f66085d2294e3e922f66545eb06aef@sentry.io/2915370',
+  dsn: process.env.SENTRY_DNS,
 };
